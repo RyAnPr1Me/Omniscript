c5281509606d5e5ab38434eae81519b6d041f35e
BIGRMAN28 <128552151+RyAnPr1Me@users.noreply.github.com>
2025-04-21 16:57:15 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,30 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  languageOptions: {
-    globals: {
-      // You can define any global variables here (if needed)
-      // e.g., process: 'readonly',
+const eslintRecommended = require('eslint/conf/eslint-recommended');
+const tsRecommended = require('@typescript-eslint/eslint-plugin').configs.recommended;
+const prettierRecommended = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    // ESLint recommended rules
+    ...eslintRecommended,
+    // TypeScript recommended rules
+    ...tsRecommended,
+    // Prettier rules to avoid conflicts
+    ...prettierRecommended,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    languageOptions: {
+      globals: {
+        // Define global variables here if needed, e.g., process: 'readonly'
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
     },
   },
-  rules: {
-    'no-console': 'warn',
-    'no-debugger': 'warn',
-  },
-};
+];
+
