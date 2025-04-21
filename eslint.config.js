544602f0879d90f172371c4c8c9f39b30a9f6593
BIGRMAN28 <128552151+RyAnPr1Me@users.noreply.github.com>
2025-04-21 16:52:26 -0400

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+  },
+};
